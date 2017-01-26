3dbb0d469486dc3fcf6ce6e426bc073680f63ba4
Iona-Kathryn Evans <ionakathryn@Iona-Kathryns-Mac-mini.local>
2017-01-26 17:10:22 +0000

    Initial commit

---
+++ app/src/main/java/com/example/android/practiceset2/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.android.practiceset2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void display (int i) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(""+i);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void incrementThreeTeamA(View view){
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void incrementTwoTeamA(View view){
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void freeThrowTeamA(View view){
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+
+    public void incrementThreeTeamB(View view){
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void incrementTwoTeamB(View view){
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void freeThrowTeamB(View view){
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+
+}
